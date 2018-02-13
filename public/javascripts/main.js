41949f229387e62388b448086c356f6ecb1f0673
naruteride <tadanide@gmail.com>
2018-02-13 13:39:24 +0900

    프런트 기본 틀 (mail-write.html 아직 다 못짬)

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+onload = function () {
+    window.addEventListener('scroll', function () {
+        var header = document.querySelector('#header');
+        var headerCover = document.querySelector('#header-cover');
+
+        if (window.scrollY >= 1) {
+            header.classList.add('on');
+            headerCover.classList.add('on');
+        } else {
+            header.classList.remove('on');
+            headerCover.classList.remove('on');
+        }
+    });
+};
